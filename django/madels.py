096ee8e75e3f08854bf484ba94e62fdcf7a61323
SuryavanshiAmruta <amrutasuryavanshi212@gmail.com>
2026-08-02 17:24:11 +0530

    corrected syntax

--- django/madels.py
+++ django/madels.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Student(models.Model):
-    student_name=models.CharField(max_length=100)
+    student_name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
     email = models.EmailField(unique=True)
     guardian_name = models.CharField(max_length=100)
